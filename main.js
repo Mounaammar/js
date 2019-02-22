65c3c86022c28472f26e2d2921c1a0e1f8e63f7e
Mounaammar <46628537+Mounaammar@users.noreply.github.com>
2019-02-22 12:17:52 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+function gras()
+{
+document.getElementById("par").style.fontWeight="bold";
+return 0;
+}
+function italic()
+{
+document.getElementById("par").style.fontStyle="italic";
+return 0;
+}
+function underline()
+{
+document.getElementById("par").style.textDecoration="underline";
+return 0;
+}
+
